03691d6eb009a65378e9a8895e42c0967152f496
Tanguy Racine <tang.mich.root@gmail.com>
2024-01-05 16:24:58 +0100

    change color scheme

--- libs/potree/potree.js
+++ libs/potree/potree.js
@@ -59038,17 +59038,13 @@ void main() {
 	const ClassificationScheme = {
 
 		DEFAULT: {
-			0:       { visible: true, name: 'never classified'  , color: [0.5,  0.5,  0.5,  1.0] },
-			1:       { visible: true, name: 'unclassified'      , color: [0.5,  0.5,  0.5,  1.0] },
-			2:       { visible: true, name: 'ground'            , color: [0.63, 0.32, 0.18, 1.0] },
-			3:       { visible: true, name: 'low vegetation'    , color: [0.0,  1.0,  0.0,  1.0] },
-			4:       { visible: true, name: 'medium vegetation' , color: [0.0,  0.8,  0.0,  1.0] },
-			5:       { visible: true, name: 'high vegetation'   , color: [0.0,  0.6,  0.0,  1.0] },
-			6:       { visible: true, name: 'building'          , color: [1.0,  0.66, 0.0,  1.0] },
-			7:       { visible: true, name: 'low point(noise)'  , color: [1.0,  0.0,  1.0,  1.0] },
-			8:       { visible: true, name: 'key-point'         , color: [1.0,  0.0,  0.0,  1.0] },
-			9:       { visible: true, name: 'water'             , color: [0.0,  0.0,  1.0,  1.0] },
-			12:      { visible: true, name: 'overlap'           , color: [1.0,  1.0,  0.0,  1.0] },
+			0:       { visible: true, name: 'top chamber'  , color: [0.66,  0.33,  0.1,  1.0] },
+			1:       { visible: true, name: 'big crystal chamber'      , color: [0.6,  0.6,  0.95,  1.0] },
+			2:       { visible: true, name: 'corkscrew'            , color: [0.63, 0.32, 0.78, 1.0] },
+			3:       { visible: true, name: 'main chamber'    , color: [0.66,  0.56,  0.3,  1.0] },
+			4:       { visible: true, name: 'small crystal chamber' , color: [0.3,  0.3,  0.8,  1.0] },
+			5:       { visible: true, name: 'traverse chamber'   , color: [0.9,  0.3,  0.0,  1.0] },
+			6:       { visible: true, name: 'sahara'          , color: [1.0,  0.66, 0.1,  1.0] },
 			DEFAULT: { visible: true, name: 'default'           , color: [0.3,  0.6,  0.6,  0.5] },
 		}
 	};
